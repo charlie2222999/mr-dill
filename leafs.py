182640491c2aadc9065e31827dad7645d0adf42e
charlie2222999 <20CWhyte.ACC@lwetb.ie>
2024-10-21 13:30:41 +0100

    Add files via upload

---
+++ leafs.py
@@ -0,0 +1,43 @@
+monday =float(input('temperature for monday'))
+tuesday =float(input('gimmie temperature for tuesday'))
+wednesday =float(input('gimmie temp for wednesday'))
+thursday =float(input('gimmie temp for thursday'))
+friday =float(input('gimmie temp for friday'))
+saturday =float(input('gimmie temp for saturday'))
+sunday =float(input('gimmie temp for sunday'))
+average_temp =monday+tuesday+wednesday+thursday+friday+saturday+sunday/7
+print(average_temp)
+x = float(input('gimmie a value for x'))
+y = float(input('gimmie a value for y'))
+z = float(input('gimmie a value for z'))
+PIE = 3.14
+ans = 4*x**4+3*y**3+9*z+PIE
+print(ans)
+user_time=float(input('gimmie a time in seconds'))
+minute=user_time//60
+second=user_time%60
+print(minute,second)
+user_num = int(input('enter an hour between 1 and 12'))
+user_ahead = int(input('enter how many hours ahead'))
+ans = user_num%user_ahead
+print(ans)
+three = int(input('enter a three digit number'))
+bobthebuilder =three//10
+sandy=three%10
+spongebob = bobthebuilder//10
+buba=bobthebuilder%10
+patrick = spongebob//10
+forest=spongebob%10
+print(sandy,buba,forest,sep='')
+day=int(input('give me a day of the month between 30'))
+month =int(input('gimmie the number of months'))
+num =month*30
+ans= day+num
+print(ans)
+
+
+
+
+               
+              
+          
