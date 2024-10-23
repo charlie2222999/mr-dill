291492222dbc738d411556d8a18427b09b38a0c1
charlie2222999 <20CWhyte.ACC@lwetb.ie>
2024-10-23 11:52:21 +0100

    Add files via upload

---
+++ pew pew.py
@@ -0,0 +1,64 @@
+x = int(input('how many days did person A work'))
+y = int(input('how many days did person B work'))
+z = int(input('how many days did person C work'))
+xy = x+y
+yz = y+z
+xz = x+z
+xyz = x*y*z
+ans =xyz/(xy+yz+xz)
+print(ans)
+x =float(input('enter an integer please'))
+y =float(input('emter an integer'))
+plus= x+y
+minus=x-y
+multi=x*y
+divid=x/y
+expo=x**y
+modo=x%y
+floor=x//y
+ans=(plus,minus,multi,divid,expo,modo,floor)
+print(ans)
+i= float(input('gimmie a number'))
+e= float(input('gimmie a number'))
+u=i
+i=e
+e=u
+print(i,u,sep='-')
+i = float(input('gimmie a number'))
+e = float(input('gimmie a number'))
+print(e,i,sep='-')
+a = float(input('gimmie a number'))
+b = float(input('gimmie a number'))
+c = float(input('gimmie a number'))
+abc = a+b+c
+ans =abc/3
+print(ans)
+r= float(input('gimmie a radius'))
+PIE = 3.14
+num = 4/3
+volume =num*PIE*r**3
+print(volume)
+name = input('gimmie name')
+age = int(input('gimmie ur age'))
+ans =100-age
+print(name,'in',ans,'years you will be 100')
+m =float(input('gimmie ur mass'))
+c = 3*108
+e=m*c**2
+print(e)
+school=input('gimmie your school')
+sn=input('gimmie ur name')
+r_num=int(input('gimmie your roll number'))
+c=input('gimmie your class number in roman numerals')
+s=input('gimmie ur section')
+a=input('gimmie ur address')
+ci=input('gimmie your city')
+pc=int(input('gimmie your pin code'))
+mum=input('gimmie your parents number')
+print('\t',school,'\t')
+print('class:',c,'\t','section',s)
+
+
+
+
+
