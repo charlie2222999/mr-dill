d35463691808a8c2b55c24df5d7464b3807ca9c6
charlie2222999 <20CWhyte.ACC@lwetb.ie>
2025-01-16 11:59:51 +0000

    Add files via upload

---
+++ new assignment.py
@@ -0,0 +1,37 @@
+temp = int(input('gimmie a temperature'))
+if temp < 32:
+    print('ice')
+elif temp < 212:
+    print('water')
+else:
+    print('steam')
+    
+x = 1
+if x > 3:
+    if x > 4:
+        print('A', end = '')
+    else :
+        print('B', end ='')
+elif x < 2:
+    if (x != 0):
+        print('C', end ='')
+print('D')
+
+weather = ('raining')
+if weather == 'sunny':
+    print('wear sunblock')
+elif weather == 'snow':
+    print('going skiing')
+else :
+    print('weather')
+
+if int('zero') == 0:
+    print('zero')
+elif str(0) =='zero' :
+    print(0)
+elif str(0) =='0' :
+    print(str(0))
+else:
+    print('none of the above')
+    
+    
