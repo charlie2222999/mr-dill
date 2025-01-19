d9ade4e1f23269e81f6d7c23dfc03426b63990ce
charlie2222999 <20CWhyte.ACC@lwetb.ie>
2025-01-19 20:01:14 +0000

    Add files via upload

---
+++ hw dill.py
@@ -0,0 +1,135 @@
+temp = int(input('gimmie a temperature'))
+if temp < 32:
+    print('ice')
+elif temp < 212:
+    print('water')
+else:
+    print('steam')
+'''    
+x = 1
+if x > 3:
+    if x > 4:
+        print('A', end = '')
+    else :
+        print('B', end ='')
+elif x < 2:
+    if (x != 0):
+        print('C', end ='')
+print('D')
+
+weather = ('raining')
+if weather == 'sunny':
+    print('wear sunblock')
+elif weather == 'snow':
+    print('going skiing')
+else :
+    print('weather')
+
+if int('zero') == 0:
+    print('zero')
+elif str(0) =='zero' :
+    print(0)
+elif str(0) =='0' :
+    print(str(0))
+else:
+    print('none of the above')
+    
+while True:
+    n = int(input('enter an int:'))
+    if n == A1 :
+        continue
+    elif n == A2 :
+        break
+    else :
+        print('what')
+else:
+    print('ever')
+    
+while True:
+    n = int(input('enter an int:'))
+    if n == A1 :
+        continue
+    elif n == A2 :
+        break
+    else :
+        print('what')
+    print('ever')
+    
+while True:
+    n = int(input('enter an int:'))
+    if n == A1:
+        continue
+    elif == A2 :
+        break
+    print('what')
+    print('ever')
+'''
+count = 0
+a = int(input('Enter a value: '))
+while a !=0:
+    count = count + 1
+a = int(input('Enter a value: '))
+print('you entered',count,'values.')
+        
+        
+count = 0
+while count <10:
+    print('Hello')
+    count +=1
+    
+x = 10
+y = 0
+while x>y:
+    print(x,y)
+    x = x-1
+    y = y+1
+    
+keepgoing = True
+x = 100
+while keepgoing:
+    print(x)
+    x = x-10
+    if x<50:
+      keepgoing = False
+
+x = 45
+while x <50:
+    print(x)
+    
+for x in[1,2,3,4,5]:
+print(x)
+
+for x in range(5):
+    print(x)
+    
+for p in range(1,10):
+    print(p)
+    
+x = 10
+y = 0
+for i in range(x-y *2):
+    print('%',i)
+    
+for x in range(3):
+    for y in range(4):
+        print(x,y,x+y)
+        
+c = 0
+for x in range(10):
+    for y in range(5):
+        c +=1
+    print(c)
+    
+for i in range(4):
+    for j in range(5):
+        if i + 1 == j or j + i ==4:
+            print('+',end = '')
+        else:
+            print('o',end = '')
+    print()
+
+
+
+    
+
+    
