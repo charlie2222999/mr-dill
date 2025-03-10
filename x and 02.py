3afc4cdf81bbc94750ba21abd859e3c6f5b4d2c8
charlie2222999 <20CWhyte.ACC@lwetb.ie>
2025-03-10 13:24:08 +0000

    Update x and 02.py

--- x and 02.py
+++ x and 02.py
@@ -12,22 +12,55 @@ print(grid2)
 grid3 = ['1','2','3']
 print(grid3)
 turns = 9
-while turn >= 1:
+
+while turns >= 1:
     player1_guess = input('enter grid1 grid2 or grid3 ensure you spell the same')
     if player1_guess == 'grid1':
         x_y = int(input('enter number 1 - 3'))
         grid1[x_y -1] = "x"
         turns = turns -1
-        print(grid1)
-    
+        print(grid1,'\n',grid2,'\n',grid3)
+        while grid1[x_y-1] == 'x' or grid1[x_y-1] =='y':
+            print('grid square already used')
+            x_y = int(input('enter number 1 - 3'))
     elif player1_guess == 'grid2':
         x_y = int(input('enter number 1 - 3'))
         grid2[x_y -1] = "x"
         turns = turns -1
-        print(grid2)
+        print(grid1,'\n',grid2,'\n',grid3)
+        while grid1[x_y-1] == 'x' or grid1[x_y-1] =='y':
+            print('grid square already used')
+            x_y = int(input('enter number 1 - 3'))
     elif player1_guess == 'grid3':
         x_y = int(input('enter number 1 - 3'))
         grid3[x_y -1] = "x"
         turns = turns -1
-        print(grid3)
-break
+        print(grid1,'\n',grid2,'\n',grid3)
+        while grid1[x_y-1] == 'x' or grid1[x_y-1] =='y':
+            print('grid square already used')
+            x_y = int(input('enter number 1 - 3'))
+    player2_guess = input('enter grid1 grid2 or grid3 ensure you spell the same')
+    if player2_guess == 'grid1':
+        x_y = int(input('enter number 1 - 3'))
+        grid1[x_y -1] = "y"
+        turns = turns -1
+        print(grid1,'\n',grid2,'\n',grid3)
+        while grid1[x_y-1] == 'x' or grid1[x_y-1] =='y':
+            print('grid square already used')
+            x_y = int(input('enter number 1 - 3'))
+    elif player2_guess == 'grid2':
+        x_y = int(input('enter number 1 - 3'))
+        grid2[x_y -1] = "y"
+        turns = turns -1
+        print(grid1,'\n',grid2,'\n',grid3)
+        while grid1[x_y-1] == 'x' or grid1[x_y-1] =='y':
+            print('grid square already used')
+            x_y = int(input('enter number 1 - 3'))
+    elif player2_guess == 'grid3':
+        x_y = int(input('enter number 1 - 3'))
+        grid3[x_y -1] = "y"
+        turns = turns -1
+        print(grid1,'\n',grid2,'\n',grid3)
+        while grid1[x_y-1] == 'x' or grid1[x_y-1] =='y':
+            print('grid square already used')
+            x_y = int(input('enter number 1 - 3'))
