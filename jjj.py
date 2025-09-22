de48bff49a1229f3f9c1338d4d51814abb3c6206
charlie2222999 <20CWhyte.ACC@lwetb.ie>
2025-09-22 22:15:02 +0100

    Add files via upload

---
+++ jjj.py
@@ -0,0 +1,41 @@
+#using pythagorus theorem
+import  math 
+def calculateLineLength(a,b):
+    #a = 3
+    #b = 4
+    lengthofside = math.sqrt(a**2+b**2)
+    return lengthofside
+print(calculateLineLength(3,4))
+#add un to an word
+#1
+def function(a):
+    addun = (a +'un')
+    return addun
+a = 'hello'
+print(function(a))
+#2
+a = input('enter a word')
+def func(a):
+    adds = (a + 's')
+    return adds
+print(func(a))
+#3
+a = float(input('enter the diameter of the circle'))
+import  math
+def ans(a):
+    r = (a/2)
+    area = math.pi*(r**2)
+    return area
+print(ans(a))
+
+
+import math 
+def ans(a,b):
+    area = math(a*b)
+    return area
+a= input('enter the height')
+b = input('enter the width')
+print((area))
+
+import math
+def 
